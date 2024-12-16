9bf86c0956047c5511710541d319b53d6b9f8ea0
yanakapylova <yana.kapylova@innowise.com>
2024-12-16 13:38:51 +0100

    Update EventItem.tsx

--- app/(platform)/schedule/EventItem.tsx
+++ app/(platform)/schedule/EventItem.tsx
@@ -20,7 +20,11 @@ export const EventItem = ({ className, index, user, item }: any) => {
               })}
             </div>
           )}
-          <div className={styles.activity}>{item.activity}</div>
+          {item.type == "permanent" && (
+            <div className={styles.activity}>
+              {activities[item.activity as keyof typeof activities]}
+            </div>
+          )}
           <div className={styles.place}>{item.place}</div>
         </div>
         <button
